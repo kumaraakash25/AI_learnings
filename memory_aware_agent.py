d286b4354ad398018daf1d65f6938a30e996a8ea
Aakash Kumar <31745143+kumaraakash25@users.noreply.github.com>
2025-04-26 20:50:07 +0530

    Update memory_aware_agent.py

--- memory_aware_agent.py
+++ memory_aware_agent.py
@@ -2,23 +2,19 @@ import os
 from mem0 import Memory
 from openai import OpenAI
 
-
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+QUADRANT_HOST = os.getenv("QUADRANT_HOST")
+NEO4J_URL= os.getenv("NEO4J_URL")
+NEO4J_USERNAME= os.getenv("NEO4J_USERNAME")
+NEO4J_PASSWORD= os.getenv("NEO4J_PASSWORD")
 
-QUADRANT_HOST = "localhost"
-
-NEO4J_URL="bolt://localhost:7687"
-NEO4J_USERNAME="neo4j"
-NEO4J_PASSWORD="password"
-
-USER_ID = "ak_001" 
-
+USER_ID = "ak007"
 
 config = {
     "version": "v1.1",
     "embedder": {
         "provider": "openai",
-        "config": {"api_key": OPENAI_API_KEY, "model": "text-embedding-3-large"},
+        "config": {"api_key": OPENAI_API_KEY, "model": "text-embedding-3-small"},
     },
     "llm": {"provider": "openai", "config": {"api_key": OPENAI_API_KEY, "model": "gpt-4.1"}},
     "vector_store": {
@@ -37,50 +33,55 @@ config = {
 mem_client = Memory.from_config(config)
 openai_client = OpenAI(api_key=OPENAI_API_KEY)
 
-
-messages = []
+def search_memories(message):
+    results = mem_client.search(message, user_id= USER_ID) 
+    memories_list = [
+        r.get('memory', '') 
+        for r in results.get('results', []) 
+        if r.get('memory') and r.get('score', 0) > 0.2
+    ]
+    memories_text = "\n".join(memories_list)
+    return memories_text
 
 def chat(message):
-    mem_result = mem_client.search(query=message, user_id=USER_ID)
-    memories = "\n".join([m["memory"] for m in mem_result.get("results")])
+    memories = search_memories(message)
 
-    SYSTEM_PROMPT = f"""
-    You are a Memory-Aware Agent.
-    
-    - You have access to previous user memories listed below.
-    - When answering the user's new message, you MUST use relevant memories if they exist.
-    - If no relevant memory matches, reason and answer normally.
-    - Always be factually accurate and avoid guessing beyond available memories.
+    SYSTEM_PROMPT =  f"""
+    You are a memory aware Agent designed to analyse input content, extract structured information and maintain a memory store. 
 
-    MEMORY CONTEXT:
-    {memories}
+    Rules: 
+    Your tone should be professional, precise and clear
+    You should use the memories that I have saved in {memories}
     """
-
-    chat_messages = [
+    messages = [
         { "role": "system", "content": SYSTEM_PROMPT },
         { "role": "user", "content": message }
     ]
 
-    result = openai_client.chat.completions.create(
+    response = openai_client.chat.completions.create(
         model="gpt-4.1",
-        messages=chat_messages
+        messages=messages
     )
 
-    assistant_reply = result.choices[0].message.content
-
-    print("Saving memory!")
-    mem_client.add(
-        [{"role": "user", "content": message}, {"role": "assistant", "content": assistant_reply}],
-            user_id=USER_ID
+    messages.append(
+        { "role": "assistant", "content": response.choices[0].message.content }
     )
 
-    print("Assistant says", assistant_reply)    
+    mem_client.add(messages, user_id =USER_ID)
 
+    return response
+
+def main():
+    print("Assistant Ready! (type 'exit' to quit)\n")
     while True:
-        message = input(">> ")
-        print("BOT: ", chat(message=message))
+        user_input = input("You: ")
+        if user_input.lower() in ["exit", "quit"]:
+            print("👋 Goodbye!")
+            break
+        response = chat(user_input)
+        response_content = response.choices[0].message.content
 
-def is_new_fact(new_text, old_memories):
-    return new_text.strip() not in old_memories.strip()
+        print(f"Assistant: {response_content}\n")
 
-chat("Hello!!")
+if __name__ == "__main__":
+    main()
